174d400c2bb19d9c892f0438fcbf43ea4b0f9931
Eryx G.F <eryxgrammaticas@gmail.com>
2026-03-19 19:51:16 +0100

    feat: add random number generation and basic game loop

--- game.py
+++ game.py
@@ -1,4 +1,8 @@
 """Contains the core game logic. The main game loop lives here: generating the secret number, managing the attempt counter, calling hint functions, checking for win or loss, returning the result. This is the largest file."""
+
+#Random module for the core function:
+import random
+
 #"Difficulty" Dictionnary that defines each level of difficulty (number of attempts, range and hint precision/thresholds):
 difficulty = {
     "easy": {
@@ -32,3 +36,31 @@ difficulty = {
         ]
     }
 }
+
+#Function that establishes the core game loop: generates random number, asks for a guess in while loop, checks if its correct, decrements attempts and detects losses:
+def core_loop(level):
+    #Generates + stores random number:
+    rand_num = random.randrange(*level["range"])
+    #Establish attempt counter:
+    attempts = level["attempts"]
+    while True:
+        #Get the user's guess
+        while True:
+            try:
+                guess = int(input("Please take a guess: "))
+                break
+            except ValueError:
+                print("This is not a valid guess. Please guess again: ")
+        #Take off guess from the attempt counter   
+        attempts -= 1
+        #Check if the users guess is right:
+        if guess == rand_num:
+            print("You have guessed correctly !")
+            return True
+        else:
+        #Check how many attempts are left
+            if attempts > 0:    
+                print("You have guessed incorrectly. Please guess again: ")
+            else:
+                print("You have exceeded the total number of guesses you had. Play another round to try your luck again.")
+                return False
